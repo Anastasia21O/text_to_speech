66eb45750cee940f41c1fc23f63ebcbb1a259292
Anastasia21O <virstanastya21@gmail.com>
2023-08-31 12:23:32 +0300

    (add) new structure

--- main.py
+++ main.py
@@ -1,74 +1,20 @@
-from io import BytesIO
-
-import pygame
 import uvicorn
 from fastapi import FastAPI
-from gtts import gTTS
 
-# import os
-# import pyglet
+from api import router
 
 app = FastAPI()
 
+app.include_router(router)
 
-def say(text):
-    tts = gTTS(text=text, lang='uk')
-    fp = BytesIO()
-    tts.write_to_fp(fp)
-    fp.seek(0)
-    pygame.mixer.init()
-    pygame.mixer.music.load(fp)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        pygame.time.Clock().tick(10)
-
-
-def bytesSpeach(text: str):
-    audio = gTTS(text=text, lang="uk")
-    fp = BytesIO()
-    audio.write_to_fp(fp)
-    fp.seek(0)
-
-    return str(fp.getvalue()), fp
-
-
-def file(text: str):
-    file_name = "example.mp3"
-
-    audio = gTTS(text=text, lang="uk")
-    audio.save(file_name)
-    # os.system("start example.mp3")
-
-    data = open(file_name, 'rb').read()
-    return file_name, str(data)
-
-
-@app.get("/text_to_speach/say")
-def text_to_speach_say(text: str):
-    pygame.init()
-    say(text)
-
-
-@app.get("/text_to_speach/bytes")
-def text_to_speach_bytes(text: str):
-    return bytesSpeach(text)
-
-
-@app.get("/text_to_speach/file")
-def text_to_speach_file(text: str):
-    return file(text)
-
+port = 8080
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8080,
+        port=port,
         log_level="debug",
         reload=True,
         workers=1,
     )
-
-# test = pyglet.media.load(None, file=fp, streaming=False)
-# test.play()
-# pyglet.app.run()
---
+++ service.py
@@ -0,0 +1,42 @@
+from io import BytesIO
+
+import pygame
+from gtts import gTTS
+
+
+# import os
+# import pyglet
+
+
+def say(text: str):
+    tts = gTTS(text=text, lang='uk')
+    fp = BytesIO()
+    tts.write_to_fp(fp)
+    fp.seek(0)
+    pygame.mixer.init()
+    pygame.mixer.music.load(fp)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+
+def bytesSpeach(text: str):
+    audio = gTTS(text=text, lang="uk")
+    fp = BytesIO()
+    audio.write_to_fp(fp)
+    fp.seek(0)
+    return str(fp.getvalue())
+
+
+def file(text: str):
+    file_name = "example.mp3"
+    audio = gTTS(text=text, lang="uk")
+    audio.save(file_name)
+
+    # Запустити mp3
+    # os.system("start example.mp3")
+
+    # витягнути байти
+    # data = open(file_name, 'rb').read()
+
+    return file_name
